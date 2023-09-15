7cef324d4a3746275af0b81b1816a76cbcf53607
vivekkeyvalue <vivek@keyvalue.systems>
2023-09-15 14:34:12 +0530

    add style function for translatex and translatey

--- src/components/Video/MyVideo.js
+++ src/components/Video/MyVideo.js
@@ -1,286 +1,100 @@
 import React from "react";
-import {
-  AbsoluteFill,
-  Img,
-  Sequence,
-  interpolate,
-  useCurrentFrame,
-  useVideoConfig,
-} from "remotion";
+import { AbsoluteFill, Img, interpolate, useCurrentFrame } from "remotion";
 import train from "./train.png";
+import pole from "./pole.png";
 
 export const MyVideo = () => {
-  const testData = {
+  const scene = {
     fps: 30,
-    scenes: [
+    durationInFrames: 30,
+    startFrame: 0,
+    endFrame: 29,
+    images: [
       {
-        durationInFrames: 30,
-        startFrame: 0,
-        endFrame: 29,
-        layers: [
-          {
-            version: "4.0",
-            name: "vertical_product_1",
-            layerNumber: 1,
-            opacity: 1,
-            rotate: 0,
-            id: "mkSJlaLnONqDJCfzcocfg",
-            type: "image",
-            layerType: "product",
-            top: 438.0934732,
-            left: 0,
-            scaleX: 1.0975609756097562,
-            scaleY: 1.0975609756097562,
-            hidden: false,
-            url: train,
-            orientation: "vertical",
-          },
-        ],
-        version: "v1",
-        id: "Z9LrskYRk4vH1L1-V_iMQ",
-        canvasBgColor: "rgb(255, 255, 255)",
+        version: "4.0",
+        name: "vertical_product_1",
+        layerNumber: 1,
+        opacity: 1,
+        rotate: 0,
+        id: "mkSJlaLnONqDJCfzcocfg",
+        type: "image",
+        layerType: "product",
+        top: 200,
+        left: 10,
+        scaleX: 1.0975609756097562,
+        scaleY: 1.0975609756097562,
+        hidden: false,
+        url: train,
+        orientation: "vertical",
+        translateX: {
+          start: 0,
+          end: 1000,
+        },
       },
       {
-        durationInFrames: 30,
-        startFrame: 30,
-        endFrame: 59,
-        layers: [
-          {
-            version: "4.0",
-            name: "vertical_product_1",
-            layerNumber: 1,
-            opacity: 1,
-            rotate: 0,
-            id: "mkSJlaLnONqDJCfzcocfg",
-            type: "image",
-            layerType: "product",
-            top: 438.0934732,
-            left: 100,
-            scaleX: 1.0975609756097562,
-            scaleY: 1.0975609756097562,
-            hidden: false,
-            url: train,
-            orientation: "vertical",
-          },
-        ],
-        version: "v1",
-        id: "Z9LrskYRk4vH1L1-V_iMQ2",
-        canvasBgColor: "rgb(255, 255, 255)",
-      },
-      {
-        durationInFrames: 30,
-        startFrame: 60,
-        endFrame: 89,
-        layers: [
-          {
-            version: "4.0",
-            name: "vertical_product_1",
-            layerNumber: 1,
-            opacity: 1,
-            rotate: 0,
-            id: "mkSJlaLnONqDJCfzcocfg",
-            type: "image",
-            layerType: "product",
-            top: 438.0934732,
-            left: 200,
-            scaleX: 1.0975609756097562,
-            scaleY: 1.0975609756097562,
-            hidden: false,
-            url: train,
-            orientation: "vertical",
-          },
-        ],
-        version: "v1",
-        id: "Z9LrskYRk4vH1L1-V_iMQ3",
-        canvasBgColor: "rgb(255, 255, 255)",
-      },
-      {
-        durationInFrames: 30,
-        startFrame: 90,
-        endFrame: 119,
-        layers: [
-          {
-            version: "4.0",
-            name: "vertical_product_1",
-            layerNumber: 1,
-            opacity: 1,
-            rotate: 0,
-            id: "mkSJlaLnONqDJCfzcocfg",
-            type: "image",
-            layerType: "product",
-            top: 438.0934732,
-            left: 300,
-            scaleX: 1.0975609756097562,
-            scaleY: 1.0975609756097562,
-            hidden: false,
-            url: train,
-            orientation: "vertical",
-          },
-        ],
-        version: "v1",
-        id: "Z9LrskYRk4vH1L1-V_iMQ3",
-        canvasBgColor: "rgb(255, 255, 255)",
-      },
-      {
-        durationInFrames: 30,
-        startFrame: 120,
-        endFrame: 149,
-        layers: [
-          {
-            version: "4.0",
-            name: "vertical_product_1",
-            layerNumber: 1,
-            opacity: 1,
-            rotate: 0,
-            id: "mkSJlaLnONqDJCfzcocfg",
-            type: "image",
-            layerType: "product",
-            top: 438.0934732,
-            left: 400,
-            scaleX: 1.0975609756097562,
-            scaleY: 1.0975609756097562,
-            hidden: false,
-            url: train,
-            orientation: "vertical",
-          },
-        ],
-        version: "v1",
-        id: "Z9LrskYRk4vH1L1-V_iMQ3",
-        canvasBgColor: "rgb(255, 255, 255)",
-      },
-      {
-        durationInFrames: 30,
-        startFrame: 150,
-        endFrame: 179,
-        layers: [
-          {
-            version: "4.0",
-            name: "vertical_product_1",
-            layerNumber: 1,
-            opacity: 1,
-            rotate: 0,
-            id: "mkSJlaLnONqDJCfzcocfg",
-            type: "image",
-            layerType: "product",
-            top: 438.0934732,
-            left: 500,
-            scaleX: 1.0975609756097562,
-            scaleY: 1.0975609756097562,
-            hidden: false,
-            url: train,
-            orientation: "vertical",
-          },
-        ],
-        version: "v1",
-        id: "Z9LrskYRk4vH1L1-V_iMQ3",
-        canvasBgColor: "rgb(255, 255, 255)",
-      },
-      {
-        durationInFrames: 30,
-        startFrame: 180,
-        endFrame: 209,
-        layers: [
-          {
-            version: "4.0",
-            name: "vertical_product_1",
-            layerNumber: 1,
-            opacity: 1,
-            rotate: 0,
-            id: "mkSJlaLnONqDJCfzcocfg",
-            type: "image",
-            layerType: "product",
-            top: 438.0934732,
-            left: 600,
-            scaleX: 1.0975609756097562,
-            scaleY: 1.0975609756097562,
-            hidden: false,
-            url: train,
-            orientation: "vertical",
-          },
-        ],
-        version: "v1",
-        id: "Z9LrskYRk4vH1L1-V_iMQ3",
-        canvasBgColor: "rgb(255, 255, 255)",
-      },
-      {
-        durationInFrames: 30,
-        startFrame: 210,
-        endFrame: 239,
-        layers: [
-          {
-            version: "4.0",
-            name: "vertical_product_1",
-            layerNumber: 1,
-            opacity: 1,
-            rotate: 0,
-            id: "mkSJlaLnONqDJCfzcocfg",
-            type: "image",
-            layerType: "product",
-            top: 438.0934732,
-            left: 700,
-            scaleX: 1.0975609756097562,
-            scaleY: 1.0975609756097562,
-            hidden: false,
-            url: train,
-            orientation: "vertical",
-          },
-        ],
-        version: "v1",
-        id: "Z9LrskYRk4vH1L1-V_iMQ3",
-        canvasBgColor: "rgb(255, 255, 255)",
+        version: "4.0",
+        name: "pole",
+        layerNumber: 1,
+        opacity: 1,
+        rotate: 0,
+        id: "mkSJlaLnONqDJCfzcocfg",
+        type: "image",
+        layerType: "product",
+        top: 100,
+        left: 500,
+        scaleX: 1.0975609756097562,
+        scaleY: 1.0975609756097562,
+        hidden: false,
+        url: pole,
+        orientation: "vertical",
       },
     ],
     version: "v1",
+    id: "Z9LrskYRk4vH1L1-V_iMQ",
+    canvasBgColor: "rgb(255, 255, 255)",
     dimensions: {
       id: 5,
       width: 1080,
       height: 1080,
+
+      durationInFrames: 500,
     },
-    durationInFrames: 500,
+  };
+  const frame = useCurrentFrame();
+
+  const getTranslateX = (val) => {
+    if (val) {
+      const translateX = interpolate(frame, [0, 200], [0, 1000]);
+      return `translateX(${translateX}px)`;
+    } else return "";
   };
 
-  const sample = [
-    {
-      version: "4.0",
-      name: "vertical_product_1",
-      layerNumber: 1,
-      opacity: 1,
-      rotate: 0,
-      id: "mkSJlaLnONqDJCfzcocfg",
-      type: "image",
-      layerType: "product",
-      top: 200,
-      left:10,
-      scaleX: 1.0975609756097562,
-      scaleY: 1.0975609756097562,
-      hidden: false,
-      url: train,
-      orientation: "vertical",
-    }
-  ]
+  const getTranslateY = (val) => {
+    if (val) {
+      const translateY = interpolate(frame, [0, 200], [0, 1000]);
+      return `translateY(${translateY}px)`;
+    } else return "";
+  };
 
-  const frame = useCurrentFrame();
-  const { durationInFrames, fps } = useVideoConfig();
-	const translateX = interpolate(
-		frame,
-		[0, 200],
-		[0, 1000]
-	);
   return (
     <>
-    <AbsoluteFill style={{backgroundColor: 'red'}}>
-
-          {sample?.map((layer) => {
-            return (
-              <AbsoluteFill
-                style={{ top: `${layer?.top}px`, left: `${layer?.left}px`,
-                transform: `translateX(${translateX}px)`  }}
-              >
-                <Img width={200} src={layer?.url} />
-              </AbsoluteFill>
-            );
-          })}
-    </AbsoluteFill>
+      <AbsoluteFill>
+        {scene.images?.map((layer) => {
+          return (
+            <AbsoluteFill
+              style={{
+                top: `${layer?.top}px`,
+                left: `${layer?.left}px`,
+                transform:
+                  getTranslateX(layer.translateX) +
+                  getTranslateY(layer.transalteY),
+              }}
+            >
+              <Img width={200} src={layer?.url} />
+            </AbsoluteFill>
+          );
+        })}
+      </AbsoluteFill>
     </>
   );
 };
